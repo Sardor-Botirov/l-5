6c763ad44fb4a97e85ad4ca9dbae29ba3edd3738
Sardor-Botirov <botirv@list.ru>
2021-09-29 15:54:18 +0500

    first commit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,75 @@
-let USERname = '   Lex Luter has a big suit       '
-let USERsurName = '   John jOnes not a good fighter   '
-let USERage = '  0.99655432  '
+let moshinBozor = [
+    {
+        name: 'malibu',
+        wheels: 4,
+        company: 'gm',
+        type: 'turbo',
+        price: {
+            ownPrice: 35000,
+            salePrice: 29000
+        }
+    },
+    {
+        name: 'tico',
+        wheels: 3,
+        company: 'daewoo',
+        type: 'turbo',
+        price: {
+            ownPrice: 2000,
+            salePrice: 1400
+        }
+    },
+    {
+        name: 'matiz',
+        wheels: 4,
+        type: 'best',
+        company: 'daewoo',
+        price: {
+            ownPrice: 3500,
+            salePrice: 2200
+        }
+    },
+    {
+        name: 'gentra',
+        wheels: 4,
+        company: 'gm',
+        type: '1.6',
+        price: {
+            ownPrice: 14000,
+            salePrice: 10000
+        }
+    },
+    {
+        name: 'velik',
+        wheels: 2,
+        company: 'merc',
+        type: 'turbo',
+        price: {
+            ownPrice: 150,
+            salePrice: 500
+        }
+    }
+]
 
-// вам нужно сделать так что бы переменные совподали 
-// использовать replace || переопределение не разрешается 
+let average = (moshinBozor[0].price.ownPrice + moshinBozor[1].price.ownPrice + moshinBozor[2].price.ownPrice + moshinBozor[3].price.ownPrice + moshinBozor[4].price.ownPrice) / 5
 
-let name = 'Alex'
-let surName = 'Jones John'
-let age = 25
+let gm = moshinBozor.filter(item => item.company === 'gm')
 
-if (USERname.slice(14, 15).toUpperCase() + USERname.slice(3, 6).toLocaleLowerCase() === name && USERsurName.slice(3, 4).toUpperCase() + USERsurName.slice(4, 5) + USERsurName.slice(10, 13) + ' ' + USERsurName.slice(3, 7) === surName && +USERage.slice(7, 8) * +USERage.slice(8, 9) === age) {
-    alert('все верно')
-} else {
-    alert('ERROR')
-}
+let cheap_gm = gm.filter(cheap => cheap.price.ownPrice <= average)
 
+console.log(average);
 
-// Cool Man
+console.log(gm)
+
+console.log(cheap_gm)
+
+// task - 2 
+
+// через фильтр найти самое длинное имя
+
+let arr = ['Alex', 'Barbara', 'john', 'michael']
+
+let longest_name = 
+
+console.log(longest_name)
+
